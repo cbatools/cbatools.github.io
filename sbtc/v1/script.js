0b59c9ad39ecb0656537ad28f60401411e8fddcf
overgd <overgd22@naver.com>
2022-08-30 21:59:09 +0900

    update : instalive 테마 사이즈 조정
    크게

--- sbtc/v1/script.js
+++ sbtc/v1/script.js
@@ -523,12 +523,11 @@ function showMessage({ chan, type, message = '', data = {}, timeout = messageTim
 
       chatEle.style.flexDirection = 'column';
       chatEle.style.justifyContent = 'flex-end';
-      chatEle.style.height = '48%';
+      chatEle.style.height = '56%';
       chatEle.style.webkitMaskImage = '-webkit-gradient(linear, 0% 50%, 0% 0%, from(rgb(0, 0, 0)), to(rgba(0, 0, 0, 0)))';
       chatEle.style.background = 'rgba(0, 0, 0, .6)';
 
       nameEle.style.color = '#fff';
-      nameEle.style.fontSize = '1em';
       chatLineInner.style.color = '#fff';
       
       heartEmojiFlow();
